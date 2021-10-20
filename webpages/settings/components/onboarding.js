1d31f356e9116bff3e2318440e9aecfb83adcc90
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-10-19 20:41:48 -0400

    New, cooler, easier, epicer system

--- webpages/settings/components/onboarding.js
+++ webpages/settings/components/onboarding.js
@@ -1,13 +1,46 @@
 export default {
   data() {
     return {
-      onboardingPage: 1,
+      page: 0,
+      data: [
+        {
+          showIcon: true,
+          title: "Welcome to a new Scratch.",
+          desc: `With over 100 customizable addons,
+          <br />
+          your Scratch experience will never be the same.`,
+          button: "Get Started!",
+        },
+        {
+          title: "Pick a theme",
+          desc: `This will affect the theme of the settings page,
+          <br />
+          popup, and other pages within the extension.`,
+          select: [
+            {
+              title: "Light",
+              img: "../../images/settings/light.svg",
+              selected: () => this.$settingsContext.theme,
+              click: () => this.$settingsContext.setTheme(true),
+            },
+            {
+              title: "Dark",
+              dark: true,
+              img: "../../images/settings/dark.svg",
+              selected: () => !this.$settingsContext.theme,
+              click: () => this.$settingsContext.setTheme(false),
+            },
+          ],
+          button: "Next",
+          buttonNote: "You can always change this later.",
+        },
+      ],
     };
   },
 
   methods: {
     nextPage() {
-      this.onboardingPage++;
+      this.page++;
     },
 
     enableAddon(addonId, enabled) {
